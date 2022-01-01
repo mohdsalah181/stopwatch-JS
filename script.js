78f11f14fe92d7399599381d60ac49cf6a4ff136
unknown <Administrator@TECH746.mtn.sd>
2022-01-01 18:36:06 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,60 @@
+window.onload = function() {
+
+    var seconds = 00;
+    var tens = 00;
+
+    var appendTens = document.getElementById("tens");
+    var appendSeconds = document.getElementById("seconds");
+
+    var buttonStart = document.getElementById("button-start");
+    var buttonStop = document.getElementById("button-stop");
+    var buttonReset = document.getElementById("button-reset");
+
+    var interval;
+
+    buttonStart.onclick = function() {
+        clearInterval(interval);
+        interval = setInterval(startTimer, 10);
+        console.log('start button')
+    }
+
+    buttonStop.onclick = function() {
+        clearInterval(interval);
+    }
+
+    buttonReset.onclick = function() {
+        clearInterval(interval);
+        tens = "00";
+        seconds = "00";
+        appendSeconds.innerHTML = seconds;
+        appendTens.innerHTML = tens;
+    }
+
+    function startTimer() {
+        tens++;
+
+        if (tens <= 9) {
+            appendTens.innerHTML = "0" + tens;
+        }
+
+        if (tens > 9){
+            appendTens.innerHTML = tens;
+        }
+
+        if (tens > 99){
+            seconds++;
+            tens = 0;
+            appendSeconds.innerHTML = "0" + seconds;
+            appendTens.innerHTML = "0" + 0;
+            
+        }
+
+        if (seconds > 9){
+            appendSeconds.innerHTML = seconds;
+        }
+        
+
+    }
+
+
+}
